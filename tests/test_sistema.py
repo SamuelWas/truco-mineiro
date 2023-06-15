037b77e6bba446ad2109bcd72214174e6fef1362
RodrigoflBraz <rodrigoflbraz@gmail.com>
2023-06-15 12:06:32 -0300

    Implementando mais testes de sistema

--- tests/test_sistema.py
+++ tests/test_sistema.py
@@ -21,3 +21,18 @@ def test_escolher_num_de_jogadores():
     
     assert 'Escolha o nome do jogador 3' not in output
     assert 'Escolha o nome do jogador 4' not in output
+
+@pytest.mark.cmd
+def test_escolher_jogar_a_carta_na_primeira_rodada():
+    path =  r"C:\Users\rodrigo.braz\Rodrigo_Si\tp_testesoftware\truco-mineiro\main.py"
+        
+    processo = subprocess.Popen(['python', path], text= True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    processo.stdin.write('2' + "\n")
+    processo.stdin.write('Rodrigo' + "\n")
+    processo.stdin.write('Andre Hora' + "\n")
+    processo.stdin.write('J' + "\n")
+
+    output, errors = processo.communicate()
+    processo.stdin.close()
+
+    assert 'Qual carta deseja Jogar? ' in output
