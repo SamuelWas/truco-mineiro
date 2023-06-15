d9e6ff3825066d82cde8100e0518e11d96a762b4
RodrigoflBraz <rodrigoflbraz@gmail.com>
2023-06-15 14:04:52 -0300

    novo teste sistema

--- tests/test_sistema.py
+++ tests/test_sistema.py
@@ -5,7 +5,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 @pytest.mark.cmd
-def test_escolher_num_de_jogadores():
+def test_escolher_2_como_num_jogadores():
     path =  r"C:\Users\rodrigo.braz\Rodrigo_Si\tp_testesoftware\truco-mineiro\main.py"
         
     processo = subprocess.Popen(['python', path], text= True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
@@ -23,16 +23,22 @@ def test_escolher_num_de_jogadores():
     assert 'Escolha o nome do jogador 4' not in output
 
 @pytest.mark.cmd
-def test_escolher_jogar_a_carta_na_primeira_rodada():
+def test_escolher_4_como_num_jogadores():
     path =  r"C:\Users\rodrigo.braz\Rodrigo_Si\tp_testesoftware\truco-mineiro\main.py"
         
     processo = subprocess.Popen(['python', path], text= True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    processo.stdin.write('2' + "\n")
+    processo.stdin.write('4' + "\n")
     processo.stdin.write('Rodrigo' + "\n")
-    processo.stdin.write('Andre Hora' + "\n")
-    processo.stdin.write('J' + "\n")
+    processo.stdin.write('Ana' + "\n")
+    processo.stdin.write('Samuel' + "\n")
+    processo.stdin.write('Heitor' + "\n")
+
 
     output, errors = processo.communicate()
     processo.stdin.close()
 
-    assert 'Qual carta deseja Jogar? ' in output
+    assert 'Escolha o nome do jogador 1' in output
+    assert 'Escolha o nome do jogador 2' in output
+    
+    assert 'Escolha o nome do jogador 3' in output
+    assert 'Escolha o nome do jogador 4' in output
