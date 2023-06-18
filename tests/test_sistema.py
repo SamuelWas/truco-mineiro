d131cbdc24f9185d3c8abb5e0eeba224c31d4a65
RodrigoFlBraz <rodrigoflbraz@gmail.com>
2023-06-18 17:36:02 -0300

    Consetando testes de sistema

--- tests/test_sistema.py
+++ tests/test_sistema.py
@@ -1,44 +1,54 @@
-# import pytest
-# import subprocess
-# import warnings
+import pytest
+import subprocess
+import warnings
+import os
 
-# warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore")
 
-# @pytest.mark.cmd
-# def test_escolher_2_como_num_jogadores():
-#     path =  r"C:\Users\rodrigo.braz\Rodrigo_Si\tp_testesoftware\truco-mineiro\main.py"
-        
-#     processo = subprocess.Popen(['python', path], text= True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-#     processo.stdin.write('2' + "\n")
-#     processo.stdin.write('Rodrigo' + "\n")
-#     processo.stdin.write('Andre Hora' + "\n")
+#prepara o path para o arquivo main.py
+@pytest.fixture
+def path():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
 
-#     output, errors = processo.communicate()
-#     processo.stdin.close()
+    parent_dir = os.path.abspath(os.path.join(current_dir, os.pardir))
 
-#     assert 'Escolha o nome do jogador 1' in output
-#     assert 'Escolha o nome do jogador 2' in output
+    path_main = os.path.join(parent_dir, "main.py")
     
-#     assert 'Escolha o nome do jogador 3' not in output
-#     assert 'Escolha o nome do jogador 4' not in output
+    return path_main
 
-# @pytest.mark.cmd
-# def test_escolher_4_como_num_jogadores():
-#     path =  r"C:\Users\rodrigo.braz\Rodrigo_Si\tp_testesoftware\truco-mineiro\main.py"
+@pytest.mark.cmd
+def test_escolher_2_como_num_jogadores(path):
         
-#     processo = subprocess.Popen(['python', path], text= True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-#     processo.stdin.write('4' + "\n")
-#     processo.stdin.write('Rodrigo' + "\n")
-#     processo.stdin.write('Ana' + "\n")
-#     processo.stdin.write('Samuel' + "\n")
-#     processo.stdin.write('Heitor' + "\n")
+    processo = subprocess.Popen(['python', path], text= True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    processo.stdin.write('2' + "\n")
+    processo.stdin.write('Rodrigo' + "\n")
+    processo.stdin.write('Andre Hora' + "\n")
 
+    output, errors = processo.communicate()
+    processo.stdin.close()
 
-#     output, errors = processo.communicate()
-#     processo.stdin.close()
-
-#     assert 'Escolha o nome do jogador 1' in output
-#     assert 'Escolha o nome do jogador 2' in output
+    assert 'Escolha o nome do jogador 1' in output
+    assert 'Escolha o nome do jogador 2' in output
     
-#     assert 'Escolha o nome do jogador 3' in output
-#     assert 'Escolha o nome do jogador 4' in output
+    assert 'Escolha o nome do jogador 3' not in output
+    assert 'Escolha o nome do jogador 4' not in output
+
+@pytest.mark.cmd
+def test_escolher_4_como_num_jogadores(path):
+        
+    processo = subprocess.Popen(['python', path], text= True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    processo.stdin.write('4' + "\n")
+    processo.stdin.write('Rodrigo' + "\n")
+    processo.stdin.write('Ana' + "\n")
+    processo.stdin.write('Samuel' + "\n")
+    processo.stdin.write('Heitor' + "\n")
+
+
+    output, errors = processo.communicate()
+    processo.stdin.close()
+
+    assert 'Escolha o nome do jogador 1' in output
+    assert 'Escolha o nome do jogador 2' in output
+    
+    assert 'Escolha o nome do jogador 3' in output
+    assert 'Escolha o nome do jogador 4' in output
